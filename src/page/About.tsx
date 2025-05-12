aae29f0a1324b16c4dd9dcc7b75bcba8ef7fde4d
pramebt <bdforwk@gmail.com>
2025-05-12 21:28:57 +0700

    error fix

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { div } from "framer-motion/client";
+
 
 const data = [
   {
