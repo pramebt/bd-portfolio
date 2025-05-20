ded7c8396c6363a9094061720b10f6b7aa59f7f9
pramebt <bdforwk@gmail.com>
2025-05-20 12:16:31 +0700

    set tag Image in footer

--- src/page/Footer.tsx
+++ src/page/Footer.tsx
@@ -22,16 +22,24 @@ const Footer = () => {
         {/* Contact info */}
         <div className="flex flex-col   space-y-2 sm:space-y-0 sm:space-x-6 text-sm">
           <a href="tel:+66994594970" className="text-white flex flex-row gap-3 hover:text-white">
-            <img 
-            src="/assets/icons/phone-icon.svg" 
-            alt="phone"
-            className='w-4' />+66 99 459 4970
+            <Image 
+              src="/assets/icons/phone-icon.svg" 
+              alt="phone"
+              width={16}
+              height={16}
+              className='w-4'
+            />
+            +66 99 459 4970
           </a>
           <a href="mailto:bdforwk@gmail.com" className="text-white flex flex-row gap-3 hover:text-white">
-            <img 
-            src="/assets/icons/email-icon.svg" 
-            alt="phone"
-            className='w-4' /> bdforwk@gmail.com
+            <Image 
+              src="/assets/icons/email-icon.svg" 
+              alt="email"
+              width={16}
+              height={16}
+              className='w-4'
+            />
+            bdforwk@gmail.com
           </a>
         </div>
 
